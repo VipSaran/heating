451c62ad7b5e9aca717db17bade27ce0027e2602
Robert Saric <robert.saric@gmail.com>
2015-01-20 17:18:46 +0100

    - create user and store hash - check hash instead of pass

--- app/user-tools.js
+++ app/user-tools.js
@@ -1,4 +1,5 @@
 var fs = require('fs');
+var bcrypt = require('bcrypt-nodejs');
 var config = require('./config-tools');
 
 var cached_users = [];
@@ -67,8 +68,7 @@ var checkCredentials = function(name, pass, cb) {
       for (var i = data.length - 1; i >= 0; i--) {
         console.log("  data[" + i + "].username=", data[i].username);
         if (data[i].username === name) {
-          // console.log("  data[" + i + "].password=", data[i].password);
-          valid = (pass == data[i].password);
+          valid = bcrypt.compareSync(pass, data[i].password);
           addCachedUser(name, valid);
           break;
         }
@@ -81,4 +81,65 @@ var checkCredentials = function(name, pass, cb) {
   }
 }
 
+var createUser = function(name, pass, cb) {
+  // console.log("user-tools.createUser()", name, pass);
+
+  bcrypt.hash(pass, null, null, function(err, hash) {
+    fs.readFile(config.app_dir + '/.auth', 'utf8', function(err, data_json) {
+      var users;
+      if (err) {
+        console.error(err);
+        users = [];
+      } else {
+        try {
+          users = JSON.parse(data_json);
+          if (users.constructor !== Array) {
+            throw new TypeError("'.auth' JSON must be an array.");
+          }
+        } catch (ex) {
+          console.error(ex);
+          // invalid JSON --> reset it
+          users = [];
+        }
+      }
+      // console.log('users=', users);
+
+      var newUser = {
+        "username": name,
+        "password": hash
+      };
+
+      var existing = false;
+      for (var i = users.length - 1; i >= 0; i--) {
+        console.log("  users[" + i + "].username=", users[i].username);
+        if (users[i].username === newUser.username) {
+          existing = true;
+          users[i] = newUser;
+          break;
+        }
+      }
+
+      if (!existing) {
+        // existing user not updated -- add new one
+        users.push(newUser);
+      }
+
+      fs.writeFile(config.app_dir + '/.auth', JSON.stringify(users), function(err) {
+        if (err) {
+          console.error(err);
+          if (typeof(cb) == "function") {
+            cb(false);
+          }
+        } else {
+          console.log("  user " + newUser.username + " successfully created and saved");
+          if (typeof(cb) == "function") {
+            cb(true);
+          }
+        }
+      });
+    });
+  });
+}
+
 exports.checkCredentials = checkCredentials;
+exports.createUser = createUser;
