61677b8238f7e3816f7c359f67af9a0fd8654f7f
Robert Saric <robert.saric@gmail.com>
2015-03-13 21:56:50 +0100

    weather: switched to yql (query in Celsius)

--- app/weather-tools.js
+++ app/weather-tools.js
@@ -1,23 +1,15 @@
 var util = require('util');
-var Forecast = require('forecast.io');
-var options = {
-  APIKey: "<ENTER_KEY_HERE>"
-};
-var forecast = new Forecast(options);
+var YQL = require('yql');
 var config = require('./config-tools');
 var logger = config.logger;
 
-var latitude = 45.5539;
-var longitude = 18.6657;
-
-var options = {
-  exclude: 'minutely,hourly,daily,flags,alerts'
-};
+var city_woeid = 849471; // Esseg, HR
+var query = new YQL('select item.condition from weather.forecast where woeid = ' + city_woeid + ' and u="c"');
 
 var getTemp = function(cb) {
   logger.info('weather-tools.getTemp()');
 
-  forecast.get(latitude, longitude, options, function(err, res, data) {
+  query.exec(function(err, data) {
     if (err) {
       logger.error(err);
 
@@ -26,17 +18,11 @@ var getTemp = function(cb) {
       }
     } else {
       try {
-        // logger.debug('  res:', util.inspect(res));
         // logger.debug('  data:', util.inspect(data));
 
-        // var tempF = data.currently.temperature;
-        // var roundTempF = tempF.toFixed(1);
+        var tempC = Number(data.query.results.channel.item.condition.temp);
 
-        var tempC = ((data.currently.temperature - 32) * 5 / 9);
         var roundTempC = parseFloat(tempC.toFixed(1));
-
-        // logger.debug('  temp (F):', roundTempF, tempF);
-        // logger.debug('  temp (C):', roundTempC, tempC);
 
         if (typeof(cb) == "function") {
           cb(roundTempC);
